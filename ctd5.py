d578f37244762b61f5ead8f87a27ed2faf5b6d11
Nadine Mohamed <96315287+Nadinegp@users.noreply.github.com>
2022-04-17 21:10:41 +0200

    Add files via upload

--- ctd5.py
+++ ctd5.py
@@ -1,76 +1,85 @@
 import random
+global bag1,bag2,bag3,bag
 bag1=10
 bag2=10
 bag3=10
+bag=[bag1 , bag2 , bag3]
 
-print(bag1,"- ",bag2," -",bag3 )
+print (bag)
 
-def check(compobj):
-    if(bag1<compobj):
-        compobj=random.randint(1, bag1)
-    elif(bag2<compobj):
-        compobj=random.randint(1, bag2)
-    elif(bag3<compobj):
-        compobj=random.randint(1, bag3)
-    
-        return compobj;
-
-def userobj():
-    try:
-            userobj=int(input("number of obj u want to remove: "))
-            if(userobj>5 or userobj<0):
-                userobj=int(input("enter a valid number: "))
-    except ValueError:
-        print("incorrect input")
-        userobj=int(input("number of bag: "))
-    return userobj
+def isEmpty(userBag):
+    if(bag[userBag] == 0):
+        return True
+    else:
+        return False
         
-def userbag():
+def tooMuch(userBag,userObj):
+    x = bag[userBag]-userObj 
+    if(x < 0):
+        return True
+    else:
+        return False 
+         
+def validateObj(noObj):
+    if(noObj<=5 and noObj>=0):
+        return noObj
+    else:
+        noObj=int(input("Pick number of objects "))
+        return noObj
+
+def validateBag(userBag):
+    try:
+        if(userBag>2 or userBag<-1):
+            userBag=int(input("Enter a valid number: "))
+    except Exception:
+        print("Incorrect input")
+        userBag=int(input("Number of bag: "))
+        validateBag(userBag)
+    return userBag     
+    
+while True:
     print("User's turn: ")
     try:
-        if(userbag>3 or userbag<0):
-            userbag=int(input("enter a valid number: "))
-    except ValueError:
-        print("incorrect input")
-        userbag=int(input("number of bag: "))
-    return userbag;
- 
-while True:
-    userbag=int(input("number of bag: "))
-    userBag = userbag()
-    userObj = userobj()
-    if(userBag==1):
-        bag1=bag1-userObj
-    elif(userBag==2):
-        bag2=bag2-userObj
-    elif(userBag==3):
-        bag3=bag3-userObj
-    print(bag1,"- ",bag2," -",bag3)
-    if bag1==0 and bag2==0 and bag3==0:
-        print("user wins")
+        userBag=int(input("Number of bag: "))
+    except Exception:
+        print("Enter a valid input: ")
+        userBag=int(input("Number of bag: "))
+    userBag-=1
+    userBag = validateBag(userBag) #chose index
+    while(isEmpty(userBag)==True):
+        userBag=int(input("Bag is empty, pick another bag: "))
+        userBag = validateBag(userBag)
+        userBag-=1
+    try:
+        userObj=int(input("Pick number of objects "))   
+    except Exception:
+        userObj=int(input(" **Enter a valid input** "))
+    userObj = validateObj(userObj)
+    while(tooMuch(userBag,userObj) == True):
+        print(" **Not enough objects in the bag** ")
+        try:
+            userObj=int(input("Pick number of objects "))
+        except Exception:
+            print("Enter a valid input: ")
+            userObj=int(input("Enter a valid input: "))
+        userObj = validateObj(userObj)
+    bag[userBag] = bag[userBag] - userObj
+    print("list ",bag)
+    if bag[0]==0 and bag[1]==0 and bag[2]==0:
+        print("User wins")
         break
     else: 
         print("-------------------------------------------")
         print("Computer's turn")
-        compbag=random.randint(1, 3)
-        compobj=random.randint(1, 5)
-        check(compobj)
-        try:
-            if(compbag==1 and bag1!=0):
-                bag1=bag1-compobj
-            elif(compbag==2 and bag2!=0):
-                bag2=bag2-compobj
-            elif(compbag==3 and bag3!=0):
-                bag3=bag3-compobj
-            print(bag1,"- ",bag2," -",bag3 )
-        except:
-            print("bag is empty")
-        if bag1==0 and bag2==0 and bag3==0:
-            print("computer wins")
+        compBag=random.randint(0, 2)
+        while(isEmpty(compBag) == True):
+            compBag = random.randint(0, 2)
+        compObj=random.randint(1, 5)
+        while(tooMuch(compBag,compObj)==True):
+            compObj=random.randint(1, 5)
+        bag[compBag]-=compObj
+        print(bag)
+        if bag[0]==0 and bag[1]==0 and bag[2]==0:
+            print("Computer wins")
             break
-                
-                    
-            
-                                
-            
-    
+        
